357c8c4d74420c5495a4dec7b28bc9f14d1cb834
ProdHallow <hallowbeatbox@gmail.com>
2025-12-04 04:19:29 -0500

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -9,20 +9,21 @@ set "COLOR_DONE=DONE"
 set "RESET="
 
 echo [%COLOR_UPDATE%] Checking for updates...
+pause
 
 set "updateURL=https://raw.githubusercontent.com/ProdHallow/installer/refs/heads/main/installer.bat"
 set "tempFile=%temp%\stereo_update.tmp"
 
 echo [%COLOR_UPDATE%] Downloading latest script from GitHub...
 curl -L "%updateURL%" -o "%tempFile%"
-if not exist "%tempFile%" (
-    echo [%COLOR_ERROR%] Failed to download update info.
-    pause
-    exit /b
-)
+echo Downloaded file: "%temp%\stereo_update.tmp"
+pause
 
-echo [%COLOR_UPDATE%] Comparing local script to GitHub version...
+echo [%COLOR_UPDATE%] Comparing scripts...
 fc "%tempFile%" "%~f0" >nul
+echo Error level after fc: %errorlevel%
+pause
+
 if %errorlevel% NEQ 0 (
     echo [%COLOR_SUCCESS%] New update found! Applying update...
     copy /y "%tempFile%" "%~f0" >nul
@@ -35,7 +36,7 @@ if %errorlevel% NEQ 0 (
     echo [%COLOR_SUCCESS%] You are already on the latest version.
     del "%tempFile%" >nul 2>&1
 )
-
+pause
 cls
 echo [%COLOR_PROCESS%] ============================
 echo [%COLOR_PROCESS%]      Discord Voice Module Auto-Fixer

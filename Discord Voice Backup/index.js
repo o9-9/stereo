cefc71b85a2ebf81e32e2f9c163e4e8bc07538a3
ProdHallow <hallowbeatbox@gmail.com>
2025-12-05 10:49:10 -0500

    Update index.js

--- Discord Voice Backup/index.js
+++ Discord Voice Backup/index.js
@@ -23,10 +23,11 @@ try {
   console.error('Failed to get data directory: ', e);
 }
 
-const useLegacyAudioDevice = true
-const audioSubsystemSelected = "legacy"
-const audioSubsystem = useLegacyAudioDevice || audioSubsystemSelected;
-const debugLogging = false;
+const defaultAudioSubsystem = process.platform === 'win32' ? 'experimental' : 'standard';
+const audioSubsystem = appSettings
+  ? appSettings.getSync('audioSubsystem', defaultAudioSubsystem)
+  : defaultAudioSubsystem;
+const debugLogging = appSettings ? appSettings.getSync('debugLogging', false) : false;
 
 function versionGreaterThanOrEqual(v1, v2) {
   const v1parts = v1.split('.').map(Number);
@@ -332,25 +333,34 @@ VoiceEngine.createSpeedTestConnectionWithOptions = function (userId, connectionO
   return bindSpeedTestConnectionInstance(instance);
 };
 
-VoiceEngine.setAudioSubsystem = function (subsystem) {
+const setAudioSubsystemInternal = function (subsystem, forceRestart) {
   if (appSettings == null) {
     console.warn('Unable to access app settings.');
     return;
   }
 
-  // TODO: With experiment controlling ADM selection, this may be incorrect since
-  // audioSubsystem is read from settings (or default if does not exists)
-  // and not the actual ADM used.
-  if (subsystem === audioSubsystem) {
-    return;
-  }
-
-  appSettings.set('audioSubsystem', "legacy");
-  appSettings.set('useLegacyAudioDevice', true);
+  appSettings.set('audioSubsystem', subsystem);
 
   if (isElectronRenderer) {
-    window.DiscordNative.app.relaunch();
+    if (forceRestart) {
+      // DANGER: any unconditional call to setAudioSubsytem will bootloop if we don't
+      // debounce noop changes.
+      if (subsystem === audioSubsystem) {
+        return;
+      }
+      window.DiscordNative.app.relaunch();
+    } else {
+      console.log(`Deferring audio subsystem switch to ${subsystem} until next restart.`);
+    }
   }
+};
+
+VoiceEngine.setAudioSubsystem = function (subsystem) {
+  setAudioSubsystemInternal(subsystem, true);
+};
+
+VoiceEngine.queueAudioSubsystem = function (subsystem) {
+  setAudioSubsystemInternal(subsystem, false);
 };
 
 VoiceEngine.setDebugLogging = function (enable) {
@@ -533,7 +543,7 @@ VoiceEngine.getNextVideoOutputFrame = function (streamId) {
 console.log(`Initializing voice engine with audio subsystem: ${audioSubsystem}`);
 VoiceEngine.platform = process.platform;
 VoiceEngine.initialize({
-  audioSubsystem: "legacy",
+  audioSubsystem,
   logLevel,
   dataDirectory,
   useFakeVideoCapture,

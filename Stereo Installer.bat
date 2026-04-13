c046586ea3bb9de4a842526a817ff146b545ae28
o9 <shno59.9009@gmail.com>
2026-04-13 18:42:56 +0300

    Delete Stereo Installer.bat

--- Stereo Installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Stereo Installer
-echo Downloading latest Stereo Installer...
-powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/ProdHallow/installer/main/DiscordVoiceFixer.ps1 | iex"

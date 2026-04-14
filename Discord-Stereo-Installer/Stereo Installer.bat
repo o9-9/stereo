fccc4556483a7073094d3544803ad77d6f25aa7d
o9 <shno59.9009@gmail.com>
2026-04-14 17:07:33 +0300

    Delete Discord-Stereo-Installer directory

--- Discord-Stereo-Installer/Stereo Installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Stereo Installer
-echo Downloading latest Stereo Installer...
-powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/o9-9/stereo/main/installer/DiscordVoiceFixer.ps1 | iex"
